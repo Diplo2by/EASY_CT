9849fa87910772ce1a5c438efda10534ade2101a
D3LTAxx <adnanyousuff89@gmail.com>
2022-04-29 07:48:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ import easyocr
 from easyocr import imgproc
 import cv2 
 
+import numpy as np
+import matplotlib.pyplot as plt
+ 
 
 # pre processing 
 rd = easyocr.Reader(['ch_sim'])
 
-path = 'load/ten.png'
+path = 'load/darshan.png'
 res1 = rd.readtext(path)
 
 img = imgproc.loadImage(path)
@@ -17,7 +20,21 @@ res2 = rd.readtext(img)
 
 print(res1[-1][2])
 print(res2[-1][2])
+data = {'Raw Image':res1[-1][2]*100, 'PreProcessed Image':res2[-1][2]*100, }
+courses = list(data.keys())
+values = list(data.values())
+fig = plt.figure(figsize = (10, 5))
+plt.bar(courses, values, color ='maroon',
+        width = 0.2,)
+plt.xlabel("Types of Images")
+plt.ylabel("Accuracy")
+plt.title("Confidence Level")
+plt.ylim(95, 100)
 
+# plt.plot(res1[-1][2],res2[-1][2])
+# plt.xticksmin(res1[-1][2], max(res1[-1][2])+1, 1.0)
+# plt.yticks([99,res2[-1][2]*100])
+plt.show()
 
 # cv2.imshow('test',cv2.cvtColor(img,cv2.COLOR_BGR2RGB) )
 # cv2.waitKey(0)
