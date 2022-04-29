1f620f1b7087de7fcd3b56a92e193d48df11e887
Diplo2by <contactdarshanv@gmail.com>
2022-04-29 07:10:39 +0530

    Added Setup.py
    added pre processing

--- setup.py
+++ setup.py
@@ -1,51 +1,24 @@
-import cv2
-import numpy as np
 import easyocr
-import matplotlib.pyplot as plt
-
-def recognize_text(img_path):
-    
-    
-    reader = easyocr.Reader(['ch_sim'])
-    return reader.readtext(img_path)
+from easyocr import imgproc
+import cv2 
 
 
-def overlay_ocr_text(img_path, save_name):
-    # loads image
-    img = cv2.imread(img_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    
-    dpi = 80
-    fig_width, fig_height = int(img.shape[0]/dpi), int(img.shape[1]/dpi)
-    plt.figure()
-    f, axarr = plt.subplots(1,2, figsize=(fig_width, fig_height)) 
-    axarr[0].imshow(img)
-    
-    # recognize text
-    result = recognize_text(img_path)
+# pre processing 
+rd = easyocr.Reader(['ch_sim'])
 
-    # if OCR prob is over 0.5, overlay bounding box and text
-    for (bbox, text, prob) in result:
-        if prob >= 0.5:
-            # display 
-            print(f'Detected text: {text} (Probability: {prob:.2f})')
+path = 'load/ten.png'
+res1 = rd.readtext(path)
 
-            # get top-left and bottom-right bbox vertices
-            (top_left, top_right, bottom_right, bottom_left) = bbox
-            top_left = (int(top_left[0]), int(top_left[1]))
-            bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
-
-            # create a rectangle for bbox display
-            cv2.rectangle(img=img, pt1=top_left, pt2=bottom_right, color=(255, 0, 0), thickness=10)
-
-            # put recognized text
-            cv2.putText(img=img, text=text, org=(top_left[0], top_left[1] - 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 0, 0), thickness=8)
-        
-    # show and save image
-    axarr[1].imshow(img)
-    plt.savefig(f'{save_name}_overlay.jpg', bbox_inches='tight')
+img = imgproc.loadImage(path)
 
 
+res2 = rd.readtext(img)
 
-im_tt = 'load/test4.png'
-overlay_ocr_text(im_tt, 'processed')
+
+print(res1[-1][2])
+print(res2[-1][2])
+
+
+# cv2.imshow('test',cv2.cvtColor(img,cv2.COLOR_BGR2RGB) )
+# cv2.waitKey(0)
+# cv2.destroyAllWindows
